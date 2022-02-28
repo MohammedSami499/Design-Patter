415cad7733da3150ded6a6a01eb78c5d1d814e81
Mohammed Sami <mshelal555@gmail.com>
2022-02-28 14:20:12 +0200

    adding the double check method

--- src/singleton/GoFSingelton.java
+++ src/singleton/GoFSingelton.java
@@ -10,24 +10,19 @@ public class GoFSingelton {
 		this.testVar = val;
 	}
 	
+	
+	// this is the double check method
 	public static GoFSingelton getInstance(String testValue) {
 		
 		//this for preventing creating 2 instance in the multi-threading case
-		GoFSingelton result = instance;
-		if(result != null)
-			return result;
-		
-		synchronized(GoFSingelton.class) {
-			if(instance == null) {
-				instance = new GoFSingelton(testValue);
-			}
-			
-			return instance;
+		if(instance == null) {
+			synchronized(GoFSingelton.class) {
+				if(instance == null) {
+					instance = new GoFSingelton(testValue);
+				}
+		}	
 		}
-		
-		
+		return instance;
 	}
-	
-	
 
 }
