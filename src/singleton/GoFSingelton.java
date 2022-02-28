7f1dd7a7a598240d7fcb923124090b68cc678cb7
Mohammed Sami <mshelal555@gmail.com>
2022-02-28 13:00:22 +0200

    adding the thread-safe Singleton with lazy loading

--- src/singleton/GoFSingelton.java
+++ src/singleton/GoFSingelton.java
@@ -3,17 +3,9 @@ package singleton;
 public class GoFSingelton {
 
 	public String testVar ;
-	public static GoFSingelton instance ;
+	public static volatile GoFSingelton instance ;
 	
 	private GoFSingelton(String val) {
-		
-		
-		// this is for slow initialization
-		try {
-			Thread.sleep(1000);	
-		}catch(InterruptedException ex) {
-			ex.printStackTrace();
-		}
 		
 		this.testVar = val;
 	}
@@ -21,11 +13,22 @@ public class GoFSingelton {
 	public static GoFSingelton getInstance(String testValue) {
 		
 		//checking wither the object is already initialized or not 
-		if(instance == null) {
-			instance = new GoFSingelton(testValue);
+		
+		GoFSingelton result = instance;
+		if(result != null)
+			return result;
+		
+		synchronized(GoFSingelton.class) {
+			if(instance == null) {
+				instance = new GoFSingelton(testValue);
+			}
+			
+			return instance;
 		}
 		
-		return instance;
+		
 	}
 	
+	
+
 }
--- src/singleton/StartPoint.java
+++ src/singleton/StartPoint.java
@@ -3,15 +3,38 @@ package singleton;
 public class StartPoint {
 
 	public static void main(String[] args) {
-		
-		GoFSingelton singelton = GoFSingelton.getInstance("First Obj!!");
-		
-		GoFSingelton singelton2 = GoFSingelton.getInstance("Secong Obj");
-		
+
 		System.out.println("\n If you see the same value, then the Singelton is successfully applied! "+"\n" + "\n else there is a problem! \n");
 		
-		System.out.println(singelton.testVar);
-		System.out.println(singelton2.testVar);
+		Thread singleton1 = new Thread(new Singleton1());
+		Thread singleton2 = new Thread(new Singleton2());
+		
+		singleton1.start();
+		singleton2.start();
+		
+	}
+	
+	
+	static class Singleton1 implements Runnable {
+
+		@Override
+		public void run() {
+			
+			GoFSingelton singelton = GoFSingelton.getInstance("First Obj!!");
+			System.out.println(singelton.testVar);
+		}
+		
+	}
+	
+	static class Singleton2 implements Runnable {
+
+		@Override
+		public void run() {
+			GoFSingelton singelton2 = GoFSingelton.getInstance("Secong Obj !!");
+			System.out.println(singelton2.testVar);
+
+		}
+		
 	}
 	
 }
